20666c95b1e3029c5219c8163129e5e4fc7b16a3
IIm1s <docyy03@gmail.com>
2026-09-23 15:49:33 +0300

    Atzimjukalkulators 2.0v

--- src/pakotne/Nolasitrezultatus.java
+++ src/pakotne/Nolasitrezultatus.java
@@ -1,5 +1,35 @@
 package pakotne;
 
-public class Nolasitrezultatus {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Nolasitrezultatus {
+	static void lasit() {
+		String rinda;
+		String galastring = "";
+		String [] Nolasitskolenuatz = null;
+		 try {
+	            BufferedReader lasa = new BufferedReader(new FileReader("studentarezultati.txt"));
+	            rinda = lasa.readLine();
+	           
+	            while(rinda != null){
+	            	rinda = lasa.readLine();
+	            	
+	            	galastring += rinda;
+	            }
+	            galastring = galastring.replace("null", "");
+	            Nolasitskolenuatz = galastring.split(",");
+	            System.out.println("-=Skolenu Atzimes=-\n");
+	            
+	            for(int i = 0; i < Nolasitskolenuatz.length; i++) {
+	            		System.out.print(Nolasitskolenuatz[i]+" ");
+
+	            }
+	            System.out.println();
+	            lasa.close();
+	        } catch (IOException e) {
+	            System.out.println("Problamas ar datni!");
+	        }
+	}
 }
--- src/pakotne/Saglabatrezultatus.java
+++ src/pakotne/Saglabatrezultatus.java
@@ -1,5 +1,22 @@
 package pakotne;
 
-public class Saglabatrezultatus {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Saglabatrezultatus {
+	static void saglabat(String[] studenti, String[] kriteriji) {
+		try {
+			BufferedWriter raksta = new BufferedWriter(new FileWriter("studentarezultati.txt"));
+			int i;
+			for(i = 0; i<studenti.length; i++) {
+            
+		   		raksta.write("\n"+studenti[i]+","+kriteriji[i]+",\n");
+
+			}
+			raksta.close();
+		        } catch (IOException e) {
+		            System.out.println("Problamas ar datni!");
+		        }
+	}
 }
